33ed3a0649c953662ea4e4dc90f884785b3fc6e0
taek <wontaek88@gmail.com>
2026-01-14 22:24:44 +0900

    Fix: PostRepository bean 등록

--- src/main/java/zero/conflict/archiview/post/infrastructure/PostRepositoryImpl.java
+++ src/main/java/zero/conflict/archiview/post/infrastructure/PostRepositoryImpl.java
@@ -1,8 +1,10 @@
 package zero.conflict.archiview.post.infrastructure;
 
+import org.springframework.stereotype.Repository;
 import zero.conflict.archiview.post.application.port.out.PostRepository;
 import zero.conflict.archiview.post.domain.Post;
 
+@Repository
 public class PostRepositoryImpl implements PostRepository {
     @Override
     public Post save(Post post) {
