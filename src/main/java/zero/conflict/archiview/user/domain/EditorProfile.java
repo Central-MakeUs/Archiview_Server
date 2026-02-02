9e89c49db3a334cfa611df6ab41c62518e8003b2
taek <wontaek88@gmail.com>
2026-02-02 14:34:07 +0900

    feat: EditorProfile 클래스에 SuperBuilder 적용

--- src/main/java/zero/conflict/archiview/user/domain/EditorProfile.java
+++ src/main/java/zero/conflict/archiview/user/domain/EditorProfile.java
@@ -15,6 +15,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import zero.conflict.archiview.global.domain.BaseTimeEntity;
 
 @Entity
@@ -22,7 +23,7 @@ import zero.conflict.archiview.global.domain.BaseTimeEntity;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Builder
+@SuperBuilder
 public class EditorProfile extends BaseTimeEntity {
 
     @Id
