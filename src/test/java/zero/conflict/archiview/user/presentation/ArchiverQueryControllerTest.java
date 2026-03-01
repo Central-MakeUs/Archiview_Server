7884b6bf4743cf8b97050ff806fac25747bfee5e
taek <wontaek88@gmail.com>
2026-03-01 16:02:09 +0900

    feat: 테스트코드 오류 수정

--- src/test/java/zero/conflict/archiview/user/presentation/ArchiverQueryControllerTest.java
+++ src/test/java/zero/conflict/archiview/user/presentation/ArchiverQueryControllerTest.java
@@ -6,15 +6,17 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import zero.conflict.archiview.ControllerTestSupport;
-import zero.conflict.archiview.user.application.archiver.query.ArchiverProfileQueryService;
-import zero.conflict.archiview.user.application.archiver.query.TrustedEditorQueryService;
+import zero.conflict.archiview.user.application.port.in.ArchiverUserUseCase;
 import zero.conflict.archiview.user.dto.ArchiverProfileDto;
+import zero.conflict.archiview.user.dto.EditorProfileDto;
 import zero.conflict.archiview.user.dto.TrustedEditorDto;
 
 import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,10 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ArchiverQueryControllerTest extends ControllerTestSupport {
 
     @MockBean
-    private ArchiverProfileQueryService archiverProfileQueryService;
-
-    @MockBean
-    private TrustedEditorQueryService trustedEditorQueryService;
+    private ArchiverUserUseCase archiverUserUseCase;
 
     @Test
     @DisplayName("아카이버 내 프로필 조회 - 성공")
@@ -38,7 +37,7 @@ class ArchiverQueryControllerTest extends ControllerTestSupport {
                 .profileImageUrl("https://example.com/profile.png")
                 .build();
 
-        given(archiverProfileQueryService.getMyProfile(org.mockito.ArgumentMatchers.any())).willReturn(response);
+        given(archiverUserUseCase.getMyProfile(any())).willReturn(response);
 
         mockMvc.perform(get("/api/v1/archivers/me/profile")
                         .with(authenticatedUser()))
@@ -57,9 +56,10 @@ class ArchiverQueryControllerTest extends ControllerTestSupport {
                 .build();
         TrustedEditorDto.ListResponse response = TrustedEditorDto.ListResponse.from(List.of(editor));
 
-        given(trustedEditorQueryService.getTrustedEditors()).willReturn(response);
+        given(archiverUserUseCase.getTrustedEditors()).willReturn(response);
 
-        mockMvc.perform(get("/api/v1/archivers/editors/trusted"))
+        mockMvc.perform(get("/api/v1/archivers/editors/trusted")
+                        .with(authenticatedUser()))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.data.editors[0].nickname").value("맛집탐방가"));
@@ -69,9 +69,40 @@ class ArchiverQueryControllerTest extends ControllerTestSupport {
     @DisplayName("믿고 먹는 에디터 조회 - mock")
     void getTrustedEditors_mock() throws Exception {
         mockMvc.perform(get("/api/v1/archivers/editors/trusted")
-                        .queryParam("useMock", "true"))
+                        .queryParam("useMock", "true")
+                        .with(authenticatedUser()))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.data.editors").isArray());
+    }
+
+    @Test
+    @DisplayName("아카이버용 에디터 프로필 조회 - 성공")
+    void getEditorProfile_success() throws Exception {
+        UUID editorId = UUID.fromString("00000000-0000-0000-0000-000000000201");
+        EditorProfileDto.ArchiverEditorProfileResponse response = EditorProfileDto.ArchiverEditorProfileResponse.mock();
+
+        given(archiverUserUseCase.getEditorProfile(any(UUID.class), eq(editorId))).willReturn(response);
+
+        mockMvc.perform(get("/api/v1/archivers/editors/{editorId}/profile", editorId)
+                        .with(authenticatedUser()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data.nickname").value("맛집탐방가"))
+                .andExpect(jsonPath("$.data.instagramId").value("editor_insta"));
+    }
+
+    @Test
+    @DisplayName("아카이버용 에디터 프로필 조회 - mock")
+    void getEditorProfile_mock() throws Exception {
+        UUID editorId = UUID.fromString("00000000-0000-0000-0000-000000000201");
+
+        mockMvc.perform(get("/api/v1/archivers/editors/{editorId}/profile", editorId)
+                        .queryParam("useMock", "true")
+                        .with(authenticatedUser()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data.nickname").value("맛집탐방가"))
+                .andExpect(jsonPath("$.data.following").value(true));
     }
 }
